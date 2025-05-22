f2fbc7a864bab3c332ef3098e244831962db36fd
MadalinTheGrey <150152239+MadalinTheGrey@users.noreply.github.com>
2025-05-22 13:44:00 +0300

    Backend: Setup API endpoints, update db, started appointment endpoints (#4)
    * Setup API endpoints routing
    
    * Updated db, started work on appointment routes and model
    
    * Fixed bug with apis (static pages were handled either way), added jsonwebtoken, started appointmentController

---
+++ models/appointmentModel.js
@@ -0,0 +1,58 @@
+const { pool } = require("../db");
+
+exports.checkAppointmentOverlap = async (date) => {
+  const query = `
+            SELECT EXISTS (
+            SELECT 1 FROM appointments
+            WHERE appointment_date > $1::timestamp - INTERVAL '59 minutes'
+            AND appointment_date < $1::timestamp + INTERVAL '59 minutes'
+            ) as "exists";
+            `;
+
+  try {
+    const result = await pool.query(query, date);
+    return result.rows[0].exists;
+  } catch (error) {
+    console.error("Error checking appointment overlap: ", error);
+    throw error;
+  }
+};
+
+exports.addAppointment = async (appointment) => {
+  const query = `
+            INSERT INTO appointments
+            (appointment_date, user_id, title, description)
+            VALUES ($1, $2, $3, $4)
+            RETURNING id;
+    `;
+  const values = [
+    appointment.appointment_date,
+    appointment.user_id,
+    appointment.title,
+    appointment.description,
+  ];
+
+  try {
+    const result = await pool.query(query, values);
+    return result.rows[0].id;
+  } catch (error) {
+    console.error("Error adding appointment: ", error);
+    throw error;
+  }
+};
+
+exports.addAppointmentFiles = async (appointment_id, appointmentFiles) => {
+  const query = `
+            INSERT INTO appointment_files
+            (appointment_id, file_path)
+            VALUES ($1, $2)
+    `;
+  try {
+    for (const file of appointmentFiles) {
+      await pool.query(query, [appointment_id, file]);
+    }
+  } catch (error) {
+    console.error("Error adding appointment files: ", error);
+    throw error;
+  }
+};
